c6a25e3b3c42334ad6f38f73f072fd0c04b4e006
HenryChadwick <hac122@ic.ac.uk>
2023-10-25 22:37:12 +0100

    Task 5

--- Dashtests.py
+++ Dashtests.py
@@ -4,20 +4,17 @@ import pytest
 from dash.testing.application_runners import import_app
 
 
-# Import your Dash app
+
 app = import_app("quantium")
 
-# Test 1: Check if the header is present
 def test_header_present(dash_duo):
     dash_duo.start_server(app)
     dash_duo.wait_for_text_to_equal("h1", "Impact of Pink Morsels Price Increase on Sales")
 
-# Test 2: Check if the visualization is present
 def test_visualization_present(dash_duo):
     dash_duo.start_server(app)
     dash_duo.wait_for_element("div#sales-chart")
 
-# Test 3: Check if the region picker is present
 def test_region_picker_present(dash_duo):
     dash_duo.start_server(app)
     dash_duo.wait_for_element("div#region-selector")
